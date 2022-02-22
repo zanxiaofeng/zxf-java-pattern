2f81eaf3e2f50f1766c1dd4218fce4f76bf3c899
davis <zanxiaofeng@163.com>
2022-02-22 20:33:39 +0800

    update

--- functional-base/src/main/java/zxf/java/functional/Currying.java
+++ functional-base/src/main/java/zxf/java/functional/Currying.java
@@ -5,8 +5,28 @@ import zxf.java.functional.checked.*;
 import java.util.function.*;
 
 public class Currying {
+    //Common
+    public static <T, U> Function<T, CheckedConsumer<U>> curryingConsumer(CheckedBiConsumer<T, U> consumer) {
+        return (t) -> {
+            return (u) -> {
+                consumer.accept(t, u);
+            };
+        };
+    }
+
     public static <T, U> CheckedConsumer<U> curryingConsumer(T t, CheckedBiConsumer<T, U> consumer) {
-        return (u) -> consumer.accept(t, u);
+        return curryingConsumer(consumer).apply(t);
+    }
+
+    //Common
+    public static <T, U, P> Function<T, Function<U, CheckedConsumer<P>>> curryingConsumer(CheckedTriConsumer<T, U, P> consumer) {
+        return (t) -> {
+            return (u) -> {
+                return (p) -> {
+                    consumer.accept(t, u, p);
+                };
+            };
+        };
     }
 
     public static <T, U, P> CheckedBiConsumer<U, P> curryingConsumer(T t, CheckedTriConsumer<T, U, P> consumer) {
@@ -14,30 +34,62 @@ public class Currying {
     }
 
     public static <T, U, P> CheckedConsumer<P> curryingConsumer(T t, U u, CheckedTriConsumer<T, U, P> consumer) {
-        return (p) -> consumer.accept(t, u, p);
+        return curryingConsumer(consumer).apply(t).apply(u);
+    }
+
+    //Common
+    public static <T, U, R> Function<T, CheckedFunction<U, R>> curryingFunction(CheckedBiFunction<T, U, R> function) {
+        return (t) -> {
+            return (u) -> {
+                return function.apply(t, u);
+            };
+        };
     }
 
     public static <T, U, R> CheckedFunction<U, R> curryingFunction(T t, CheckedBiFunction<T, U, R> function) {
-        return (u) -> {
-            return function.apply(t, u);
+        return curryingFunction(function).apply(t);
+    }
+
+    //Common
+    public static <T, U, P, R> Function<T, Function<U, CheckedFunction<P, R>>> curryingFunction(CheckedTriFunction<T, U, P, R> function) {
+        return (t) -> {
+            return (u) -> {
+                return (p) -> {
+                    return function.apply(t, u, p);
+                };
+            };
         };
     }
 
     public static <T, U, P, R> CheckedBiFunction<U, P, R> curryingFunction(T t, CheckedTriFunction<T, U, P, R> function) {
-        return (u, p) -> {
-            return function.apply(t, u, p);
-        };
+        return (u, p) -> function.apply(t, u, p);
     }
 
     public static <T, U, P, R> CheckedFunction<P, R> curryingFunction(T t, U u, CheckedTriFunction<T, U, P, R> function) {
-        return (p) -> {
-            return function.apply(t, u, p);
+        return curryingFunction(function).apply(t).apply(u);
+    }
+
+    //Common
+    public static <T, U> Function<T, Predicate<U>> curryingPredicate(BiPredicate<T, U> predicate) {
+        return (t) -> {
+            return (u) -> {
+                return predicate.test(t, u);
+            };
         };
     }
 
     public static <T, U> Predicate<U> curryingPredicate(T t, BiPredicate<T, U> predicate) {
-        return (u) -> {
-            return predicate.test(t, u);
+        return curryingPredicate(predicate).apply(t);
+    }
+
+    //Common
+    public static <T, U, P> Function<T, Function<U, Predicate<P>>> curryingPredicate(TriPredicate<T, U, P> predicate) {
+        return (t) -> {
+            return (u) -> {
+                return (p) -> {
+                    return predicate.test(t, u, p);
+                };
+            };
         };
     }
 
@@ -48,8 +100,6 @@ public class Currying {
     }
 
     public static <T, U, P> Predicate<P> curryingPredicate(T t, U u, TriPredicate<T, U, P> predicate) {
-        return (p) -> {
-            return predicate.test(t, u, p);
-        };
+        return curryingPredicate(predicate).apply(t).apply(u);
     }
 }
